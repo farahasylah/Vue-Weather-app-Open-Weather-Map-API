e3483273e42f12c5761a0fc64bbc4ab2b2ed3873
Farah Asylah <farahasylah@gmail.com>
2022-02-24 21:48:29 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
   }, 
   data() {
     return {
-      api_key: '207a1e6a9123b10b4ae0a07ca97f6692',
+      api_key: '',
       url_base: 'http://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {},
